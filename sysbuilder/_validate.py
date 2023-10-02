33db635ab48f1816abaa2c3bef299ebc9f6d2f42
William Sellitti <william.sellitti@gmail.com>
2023-10-01 21:28:34 -0400

    validate layout

--- sysbuilder/_validate.py
+++ sysbuilder/_validate.py
@@ -11,24 +11,91 @@ validate_json = {
                 "disk": {
                     "type": "object",
                     "properties": {
-                        "ptable": {"type": "string", "enum": ["gpt"]},
+                        "ptable": {
+                            "type": "string",
+                            "enum": [
+                                "gpt",
+                            ],
+                        },
                         "path": {"type": "string"},
                         "type": {
                             "type": "string",
-                            "enum": ["physical", "sparse", "raw"],
+                            "enum": [
+                                "physical",
+                                "sparse",
+                                "raw",
+                            ],
                         },
                         "size": {
                             "type": "string",
                             "pattern": "[0-9]+([KMGTP]|$)",
                         },
                     },
-                    "required": ["path", "ptable", "type"],
+                    "required": [
+                        "path",
+                        "ptable",
+                        "type",
+                    ],
                 },
-                "layout": {"type": "array", "minItems": 1, "maxItems": 128},
+                "layout": {
+                    "type": "array",
+                    "minItems": 1,
+                    "maxItems": 128,
+                    "contains": {
+                        "type": "object",
+                        "properties": {
+                            "start": {
+                                "type": "string",
+                            },
+                            "end": {"type": "string"},
+                            "typecode": {
+                                "type": "string",
+                            },
+                            "filesystem": {
+                                "type": "object",
+                                "required": [
+                                    "type",
+                                    "mountpoint",
+                                ],
+                                "properties": {
+                                    "type": {
+                                        "type": "string",
+                                    },
+                                    "mountpoint": {
+                                        "type": "string",
+                                    },
+                                    "label": {
+                                        "type": "string",
+                                    },
+                                    "label_flag": {
+                                        "type": "string",
+                                    },
+                                    "args": {
+                                        "type": "array",
+                                    },
+                                },
+                            },
+                        },
+                        "required": [
+                            "start",
+                            "end",
+                            "typecode",
+                            "filesystem",
+                        ],
+                    },
+                },
             },
-            "required": ["disk", "layout"],
+            "required": [
+                "disk",
+                "layout",
+            ],
         },
-        "install": {"type": "object"},
+        "install": {
+            "type": "object",
+        },
     },
-    "required": ["storage", "install"],
+    "required": [
+        "storage",
+        "install",
+    ],
 }
