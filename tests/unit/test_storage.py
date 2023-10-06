22bad0d20d275d1142c2cf256d1c0e6402620a75
Bill <william.sellitti@gmail.com>
2023-10-05 22:26:03 -0400

    bad

--- tests/unit/test_storage.py
+++
@@ -1,9 +0,0 @@
-"""Storage module testing."""
-
-import unittest
-from sysbuilder.storage import _BlockDevice as BlockDevice
-
-
-class BlockDevTest(unittest.TestCase):
-    def test_get_child_devices(self):
-        """Test _BlockDevice.get_child_devices()"""
