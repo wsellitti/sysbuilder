a3992dd0782c3584b1dc38bb7950315455c79cd3
Bill <william.sellitti@gmail.com>
2023-10-28 15:07:25 -0400

    test partitioning

--- tests/integration/test_storage.py
+++ tests/integration/test_storage.py
@@ -1,5 +1,7 @@
 """Test storage."""
 
+# pylint: disable=W0212,W0201
+
 import tempfile
 import unittest
 from sysbuilder.config import Config
@@ -23,6 +25,7 @@ class SparseStorageTesting(unittest.TestCase):
         """
 
         self.vdi._device.unmount()
+        storage._LoopDevice.detach(self.vdi._device.path)
 
     def test_sparse_disk_creation(self):
         """Test creating a sparse loop file."""
@@ -56,11 +59,51 @@ class SparseStorageTesting(unittest.TestCase):
                         "type": "sparse",
                         "ptable": "gpt",
                         "size": "32G",
-                    }
+                    },
+                    "layout": [
+                        {
+                            "start": "",
+                            "end": "+100M",
+                            "typecode": "ef00",
+                            "filesystem": {
+                                "type": "vfat",
+                                "label": "EFI",
+                                "args": ["-F", "32"],
+                                "label_flag": "-n",
+                                "mountpoint": "/efi",
+                            },
+                        },
+                        {
+                            "start": "",
+                            "end": "+4G",
+                            "typecode": "8200",
+                            "filesystem": {
+                                "type": "swap",
+                                "label": "swap",
+                                "mountpoint": "swap",
+                            },
+                        },
+                        {
+                            "start": "",
+                            "end": "",
+                            "typecode": "8300",
+                            "filesystem": {
+                                "type": "ext4",
+                                "label": "root",
+                                "mountpoint": "/",
+                            },
+                        },
+                    ],
                 }
             },
         )
 
         self.vdi = storage.Storage(storage=cfg.get("storage"))
         self.vdi.format()
-        self.assertTrue(self.vdi._device.back_path == self.img_path)
+
+        print(self.vdi._device._data)
+
+        # There need to be as many partitions as were described in the configuration.
+        self.assertEqual(
+            len(self.vdi._device._children), len(cfg.get("storage")["layout"])
+        )
