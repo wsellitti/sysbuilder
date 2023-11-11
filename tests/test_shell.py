52baf294665235a6aed2dafb8bfeab9d204fef27
Bill <william.sellitti@gmail.com>
2023-11-10 23:16:46 -0500

    ddtest

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -2,12 +2,44 @@
 
 # pylint: disable=C0103
 
-from jsonschema import validate
 import os
 import tempfile
 import unittest
+from jsonschema import validate
 from sysbuilder.shell import dd, lsblk
 from tests.data.lsblk_validate import validate_json
+
+
+class ddTest(unittest.TestCase):
+    """Test instances of dd command."""
+
+    def setUp(self):
+        """Test path."""
+        self.file = os.path.join(tempfile.mkdtemp(), "disk.img")
+
+    def tearDown(self):
+        """Clean up"""
+
+        os.remove(self.file)
+        os.removedirs(os.path.dirname(self.file))
+
+    def test_dd_sparse(self):
+        """Test sparse file creation."""
+
+        dd(self.file, count="2048", convs=["sparse"])
+
+        blocks_utilized = os.stat(self.file).st_blocks
+
+        self.assertEqual(blocks_utilized * 512, 0)
+
+    def test_dd(self):
+        """Test sparse file creation."""
+
+        dd(self.file, count="2048")
+
+        blocks_utilized = os.stat(self.file).st_blocks
+
+        self.assertEqual(blocks_utilized * 512, 2147483648)
 
 
 class lsblkTest(unittest.TestCase):
