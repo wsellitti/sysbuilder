802b7815139ff2b2de81de8ad768ba10577aae76
Bill <william.sellitti@gmail.com>
2023-11-11 20:46:52 -0500

    sgdisk as a class instead of as a function

--- sysbuilder/shell.py
+++ sysbuilder/shell.py
@@ -168,63 +168,69 @@ def partprobe(*args):
     subprocess.run(command, check=True, capture_output=True)
 
 
-def sgdisk(devpath: str, layout: Dict[str, Any]) -> None:
-    """
-    sgdisk wrapper
+class SGDisk(_Shell):
+    """Wraps `sgdisk` shell command."""
 
-    # Params
+    @_Shell.command
+    def run(self, devpath: str, layout: Dict[str, Any]) -> None:
+        """
+        sgdisk wrapper
 
-      - devpath (str): Path to the device.
-      - layout (list): How the partitions should be laid out on the disk
-        provided by `devpath`. Each item represents a different partition.
+        # Params
 
-    ## Layout
+          - devpath (str): Path to the device.
+          - layout (list): How the partitions should be laid out on the disk
+            provided by `devpath`. Each item represents a different partition.
 
-    Each object provided by `layout` must contain at least one of the
-    following sets of keys (the name of the set is not a value that needs to
-    be provided, the type will be determined based on the keys in the
-    dictionary).
+        ## Layout
 
-    Partition:
-      - part_number (str): The partition number.
-      - start_sector (str): The on-disk sector a partition should start at.
-        This can be an absolute sector number or a relative value measured in
-        kibibytes, mebibytes, gibibytes, tebibytes, or prebibytes.
-      - end_sector (str): The on-disk sector a partition should end at. This
-        can be an absolute sector number or a relative value measured in
-        kibibytes, mebibytes, gibibytes, tebibytes, or prebibytes.
+        Each object provided by `layout` must contain at least one of the
+        following sets of keys (the name of the set is not a value that needs to
+        be provided, the type will be determined based on the keys in the
+        dictionary).
 
-    Typecode:
-      - part_number (int): The partition number.
-      - typecode (str): A 4-character hexadecimal value representing
-        filesystem type codes.
-    """
+        Partition:
+          - part_number (str): The partition number.
+          - start_sector (str): The on-disk sector a partition should start at.
+            This can be an absolute sector number or a relative value measured in
+            kibibytes, mebibytes, gibibytes, tebibytes, or prebibytes.
+          - end_sector (str): The on-disk sector a partition should end at. This
+            can be an absolute sector number or a relative value measured in
+            kibibytes, mebibytes, gibibytes, tebibytes, or prebibytes.
 
-    if stat.S_ISBLK(os.stat(devpath).st_mode) == 0:
-        raise ValueError(f"{devpath} is not a device file.")
+        Typecode:
+          - part_number (int): The partition number.
+          - typecode (str): A 4-character hexadecimal value representing
+            filesystem type codes.
+        """
 
-    command = ["sgdisk"]
+        if stat.S_ISBLK(os.stat(devpath).st_mode) == 0:
+            raise ValueError(f"{devpath} is not a device file.")
 
-    for flag_dict in layout:
-        part_num = flag_dict["part_num"]
-        start_sector = flag_dict.get("start_sector")
-        end_sector = flag_dict.get("end_sector")
-        typecode = flag_dict.get("typecode")
-        if start_sector is not None and end_sector is not None:
-            addon = [
-                "--new",
-                ":".join([part_num, str(start_sector), str(end_sector)]),
-            ]
-        elif typecode is not None:
-            addon = [
-                "--typecode",
-                ":".join([part_num, typecode]),
-            ]
-        else:
-            raise KeyError(
-                "Either start_sector and end_sector or typecode must be provided!"
-            )
-        command.extend(addon)
-        command.append(devpath)
+        command = ["sgdisk"]
 
-    subprocess.run(command, check=True, capture_output=True, encoding="utf-8")
+        for flag_dict in layout:
+            part_num = flag_dict["part_num"]
+            start_sector = flag_dict.get("start_sector")
+            end_sector = flag_dict.get("end_sector")
+            typecode = flag_dict.get("typecode")
+            if start_sector is not None and end_sector is not None:
+                addon = [
+                    "--new",
+                    ":".join([part_num, str(start_sector), str(end_sector)]),
+                ]
+            elif typecode is not None:
+                addon = [
+                    "--typecode",
+                    ":".join([part_num, typecode]),
+                ]
+            else:
+                raise KeyError(
+                    "Either start_sector and end_sector or typecode must be provided!"
+                )
+            command.extend(addon)
+            command.append(devpath)
+
+        subprocess.run(
+            command, check=True, capture_output=True, encoding="utf-8"
+        )
